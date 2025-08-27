18b7e806292c424c2d030b76c9b9874ca591cf66
Kouta <koutasms@gmail.com>
2025-08-27 12:17:55 +0200

    version final 1.1

--- data/enemigos.js
+++ data/enemigos.js
@@ -1,37 +1,37 @@
 // data/enemigos.js
 export const ENEMIGOS = {
   cave1: [
-    { nombre: "icon21", sprite: "assets/enemies/cave1/icon21.png", vida: 40, daño: 3, oro: 10 },
-    { nombre: "icon22", sprite: "assets/enemies/cave1/icon22.png", vida: 50, daño: 4, oro: 12 },
-    { nombre: "icon23", sprite: "assets/enemies/cave1/icon23.png", vida: 60, daño: 5, oro: 13 },
-    { nombre: "icon25", sprite: "assets/enemies/cave1/icon25.png", vida: 70, daño: 5, oro: 15 }
+    { nombre: "icon21", sprite: "assets/enemies/cave1/Icon21.png", vida: 40, daño: 3, oro: 10 },
+    { nombre: "icon22", sprite: "assets/enemies/cave1/Icon22.png", vida: 50, daño: 4, oro: 12 },
+    { nombre: "icon23", sprite: "assets/enemies/cave1/Icon23.png", vida: 60, daño: 5, oro: 13 },
+    { nombre: "icon25", sprite: "assets/enemies/cave1/Icon25.png", vida: 70, daño: 5, oro: 15 }
   ],
 
   cave2: [
-    { nombre: "icon1", sprite: "assets/enemies/cave2/icon1.png", vida: 80, daño: 7, oro: 15 },
-    { nombre: "icon3", sprite: "assets/enemies/cave2/icon3.png", vida: 90, daño: 8, oro: 17 },
-    { nombre: "icon7", sprite: "assets/enemies/cave2/icon7.png", vida: 100, daño: 9, oro: 18 },
-    { nombre: "icon10", sprite: "assets/enemies/cave2/icon10.png", vida: 110, daño: 10, oro: 20 }
+    { nombre: "icon1", sprite: "assets/enemies/cave2/Icon1.png", vida: 80, daño: 7, oro: 15 },
+    { nombre: "icon3", sprite: "assets/enemies/cave2/Icon3.png", vida: 90, daño: 8, oro: 17 },
+    { nombre: "icon7", sprite: "assets/enemies/cave2/Icon7.png", vida: 100, daño: 9, oro: 18 },
+    { nombre: "icon10", sprite: "assets/enemies/cave2/Icon10.png", vida: 110, daño: 10, oro: 20 }
   ],
 
   dung1: [
-    { nombre: "icon32", sprite: "assets/enemies/dung1/icon32.png", vida: 120, daño: 12, oro: 20 },
-    { nombre: "icon35", sprite: "assets/enemies/dung1/icon35.png", vida: 130, daño: 13, oro: 22 },
-    { nombre: "icon36", sprite: "assets/enemies/dung1/icon36.png", vida: 140, daño: 14, oro: 23 },
-    { nombre: "icon38", sprite: "assets/enemies/dung1/icon38.png", vida: 150, daño: 15, oro: 25 }
+    { nombre: "icon32", sprite: "assets/enemies/dung1/Icon32.png", vida: 120, daño: 12, oro: 20 },
+    { nombre: "icon35", sprite: "assets/enemies/dung1/Icon35.png", vida: 130, daño: 13, oro: 22 },
+    { nombre: "icon36", sprite: "assets/enemies/dung1/Icon36.png", vida: 140, daño: 14, oro: 23 },
+    { nombre: "icon38", sprite: "assets/enemies/dung1/Icon38.png", vida: 150, daño: 15, oro: 25 }
   ],
 
   dung2: [
-    { nombre: "icon6", sprite: "assets/enemies/dung2/icon6.png", vida: 160, daño: 16, oro: 25 },
-    { nombre: "icon7", sprite: "assets/enemies/dung2/icon7.png", vida: 170, daño: 17, oro: 27 },
-    { nombre: "icon39", sprite: "assets/enemies/dung2/icon39.png", vida: 180, daño: 18, oro: 28 },
-    { nombre: "icon43", sprite: "assets/enemies/dung2/icon43.png", vida: 200, daño: 20, oro: 30 }
+    { nombre: "icon6", sprite: "assets/enemies/dung2/Icon6.png", vida: 160, daño: 16, oro: 25 },
+    { nombre: "icon7", sprite: "assets/enemies/dung2/Icon7.png", vida: 170, daño: 17, oro: 27 },
+    { nombre: "icon39", sprite: "assets/enemies/dung2/Icon39.png", vida: 180, daño: 18, oro: 28 },
+    { nombre: "icon43", sprite: "assets/enemies/dung2/Icon43.png", vida: 200, daño: 20, oro: 30 }
   ],
 
   graveyard: [
-    { nombre: "icon14", sprite: "assets/enemies/cementerio/icon14.png", vida: 220, daño: 22, oro: 30 },
-    { nombre: "icon24", sprite: "assets/enemies/cementerio/icon24.png", vida: 240, daño: 24, oro: 32 },
-    { nombre: "icon35", sprite: "assets/enemies/cementerio/icon35.png", vida: 260, daño: 26, oro: 35 },
-    { nombre: "icon38", sprite: "assets/enemies/cementerio/icon38.png", vida: 300, daño: 30, oro: 40 }
+    { nombre: "icon14", sprite: "assets/enemies/cementerio/Icon14.png", vida: 220, daño: 22, oro: 30 },
+    { nombre: "icon24", sprite: "assets/enemies/cementerio/Icon24.png", vida: 240, daño: 24, oro: 32 },
+    { nombre: "icon35", sprite: "assets/enemies/cementerio/Icon35.png", vida: 260, daño: 26, oro: 35 },
+    { nombre: "icon38", sprite: "assets/enemies/cementerio/Icon38.png", vida: 300, daño: 30, oro: 40 }
   ]
 };
